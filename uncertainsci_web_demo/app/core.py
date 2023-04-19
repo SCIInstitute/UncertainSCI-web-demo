70d9b13cef40f7bb433b474dfe5da0f4bddda610
jessdtate <jess@sci.utah.edu>
2023-04-18 21:29:59 -0600

    building

--- uncertainsci_web_demo/app/core.py
+++ uncertainsci_web_demo/app/core.py
@@ -17,6 +17,8 @@ from UncertainSCI.pce import PolynomialChaosExpansion
 
 from UncertainSCI.vis import piechart_sensitivity, quantile_plot, mean_stdev_plot
 
+#import jsonpickle
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -45,7 +47,12 @@ class PCE_Model:
     def set_x(self, x):
         self.x = x
         
-#pce = PCE_Model()
+    def set(self, attr, value):
+        eval(attr+" = "+value)
+        
+pce_model = PCE_Model()
+        
+    
 
 class Engine:
     def __init__(self, server=None):
@@ -65,6 +72,9 @@ class Engine:
 
         N = 100
         x, model = laplace_ode_1d(Nparams, N=N)
+        
+        pce_model.set_x(x)
+        
 
         pce = PolynomialChaosExpansion(distribution=[p1, p2, p3], order=order, plabels=plabels)
         pce.generate_samples()
@@ -76,7 +86,8 @@ class Engine:
             model_output[ind, :] = model(pce.samples[ind, :])
         pce.build(model_output=model_output)
             
-
+        pce_model.set_pce(pce)
+        
         self._server = server
 
         # initialize state + controller
@@ -85,16 +96,17 @@ class Engine:
         # Set state variable
         state.trame__title = "UncertainSCI-web-demo"
         state.distribution = "beta"
-        state.model = "lapace-ode-1d"
+        state.model_name = "lapace-ode-1d"
         state.alpha = [ 0.5, 1, 1]
         state.beta = [ 1, 0.5, 1]
-        state.order = order
-        state.model_output = model_output
-        state.Nparams = Nparams
-        state.x = x
-        state.plabels = plabels
-        state.samples = pce.samples
-        state.pce = pce
+#        state.order = order
+#        state.model_output = model_output.tolist()
+#        state.Nparams = Nparams
+#        state.x = x.tolist()
+#        state.plabels = plabels
+#        state.samples = pce.samples.tolist()
+#        state.pce = jsonpickle.encode(pce)
+        state.active_plot = "Mean and Std"
 
         # Bind instance methods to controller
         ctrl.reset_distribution = self.reset_distribution
@@ -106,6 +118,8 @@ class Engine:
 
         # Bind instance methods to state change
         state.change("active_plot")(self.update_plot)
+        print("binding")
+        print(state.active_plot)
 
         # Generate UI
         self.ui()
@@ -137,9 +151,11 @@ class Engine:
         self._server.state.alpha = [ 0.5, 1, 1]
         self._server.state.beta = [ 1, 0.5, 1]
     
-    def update_plot(active_plot, **kwargs):
-        ctrl.figure_update(PLOTS[active_plot]())
-        logger.info(f">>> ENGINE(a): updating plot to {active_plot}")
+    def update_plot(self, **kwargs):
+        print("update plot")
+        print(self._server.state.active_plot)
+        self._server.controller.figure_update(PLOTS[self._server.state.active_plot]())
+        logger.info(f">>> ENGINE(a): updating plot to {self._server.state.active_plot}")
         
     def widget_click(self):
         logger.info(">>> ENGINE(a): Widget Click")
@@ -153,8 +169,10 @@ class Engine:
             layout.title.set_text("UncertainSCI demo")
             with layout.toolbar:
                 vuetify.VSpacer()
+                print("toolbar")
+                print(self._server.state.active_plot)
                 vuetify.VSelect(
-                    v_model=("active_plot", "MeanStd"),
+                    v_model=("active_plot", "Mean and Std"),
                     items=("plots", list(PLOTS.keys())),
                     hide_details=True,
                     dense=True,
@@ -162,18 +180,20 @@ class Engine:
 
             # Main content
             with layout.content:
-                with vuetify.VContainer(fluid=True, classes="pa-0 fill-height"):
+                with vuetify.VContainer(fluid=True):
                     with vuetify.VRow(dense=True):
                         vuetify.VSpacer()
-                        html_plot = plotly.Figure(
+                        print("content")
+                        print(self._server.state.active_plot)
+                        figure = plotly.Figure(
                             display_logo=False,
                             display_mode_bar=("true",),
-                            selected=(on_event, "['selected', VuePlotly.safe($event)]"),
+#                            selected=(on_event, "['selected', VuePlotly.safe($event)]"),
                             # hover=(on_event, "['hover', VuePlotly.safe($event)]"),
                             # selecting=(on_event, "['selecting', $event]"),
                             # unhover=(on_event, "['unhover', $event]"),
                         )
-                        self.ctrl.figure_update = html_plot.update
+                        self.ctrl.figure_update = figure.update
                         vuetify.VSpacer()
 
             # Footer
@@ -182,10 +202,10 @@ class Engine:
     
 def MeanStd():
 
-    mean = pce_ctrl.pce.mean()
-    stdev = pce_ctrl.pce.stdev()
+    mean = pce_model.pce.mean()
+    stdev = pce_model.pce.stdev()
 
-    x_rev = pce_ctrl.x[::-1]
+    x_rev = pce_model.x[::-1]
     upper = mean+stdev
     lower = mean-stdev
     lower=lower[::- 1]
@@ -193,7 +213,7 @@ def MeanStd():
     fig = go.Figure()
 
     fig.add_trace(go.Scatter(
-        x=np.hstack((pce_ctrl.x,x_rev)),
+        x=np.hstack((pce_model.x,x_rev)),
         y=np.hstack((upper,lower)),
         fill='toself',
         fillcolor='rgba(0,100,80,0.2)',
@@ -202,57 +222,41 @@ def MeanStd():
         name='Stdev',
     ))
     fig.add_trace(go.Scatter(
-        x=pce_ctrl.x, y=mean,
+        x=pce_model.x, y=mean,
         line_color='rgb(0,100,80)',
         name='mean',
     ))
 
     fig.update_traces(mode='lines')
-    
+
     return fig
-
-def protocols_ready():
-    print(">>> ENGINE: Server protocols initialized / Client not connected yet")
-    update_plot("MeanStd")
-    return True
-
-def reset_resolution():
-    state.resolution = 6
-    
-def widget_click():
-    print(">>> ENGINE: Widget Click")
-    update_plot()
-
-def widget_change():
-    print(">>> ENGINE: Widget Change")
-    update_plot()
 
 # -----------------------------------------------------------------------------
 
 
 def Quantiles():
-    
+
     bands = 3
     band_mass = 1/(2*(bands+1))
-    x_rev = pce_ctrl.x[::-1]
-    
+    x_rev = pce_model.x[::-1]
+
     dq = 0.5/(bands+1)
     q_lower = np.arange(dq, 0.5-1e-7, dq)[::-1]
     q_upper = np.arange(0.5 + dq, 1.0-1e-7, dq)
     quantile_levels = np.append(np.concatenate((q_lower, q_upper)), 0.5)
 
-    quantiles = pce_ctrl.pce.quantile(quantile_levels, M=int(2e3))
+    quantiles = pce_model.pce.quantile(quantile_levels, M=int(2e3))
     median = quantiles[-1, :]
-    
+
     fig = go.Figure()
-        
+
     for ind in range(bands):
         alpha = (bands-ind) * 1/bands - (1/(2*bands))
         upper = quantiles[ind, :]
         lower = quantiles[bands+ind, ::-1]
         if ind == 0:
             fig.add_trace(go.Scatter(
-                x=np.hstack((pce_ctrl.x,x_rev)),
+                x=np.hstack((pce_model.x,x_rev)),
                 y=np.hstack((upper,lower)),
                 fill='toself',
                 fillcolor='rgba(100,0,0,'+str(alpha)+')',
@@ -262,7 +266,7 @@ def Quantiles():
             ))
         else:
             fig.add_trace(go.Scatter(
-                x=np.hstack((pce_ctrl.x,x_rev)),
+                x=np.hstack((pce_model.x,x_rev)),
                 y=np.hstack((upper,lower)),
                 fill='toself',
                 fillcolor='rgba(100,0,0,'+str(alpha)+')',
@@ -270,9 +274,9 @@ def Quantiles():
                 showlegend=False,
             ))
 
-    
+
     fig.add_trace(go.Scatter(
-        x=pce_ctrl.x, y=median,
+        x=pce_model.x, y=median,
         line_color='rgb(0,0,0)',
         name='median',
     ))
@@ -288,10 +292,10 @@ def Quantiles():
 
 def SensitivityPiechart():
 
-    global_sensitivity, variable_interactions = pce_ctrl.pce.global_sensitivity()
+    global_sensitivity, variable_interactions = pce_model.pce.global_sensitivity()
     scalarized_GSI = np.mean(global_sensitivity, axis=1)
     print(type(scalarized_GSI))
-    labels = [' '.join([pce_ctrl.pce.plabels[v] for v in varlist]) for varlist in variable_interactions]
+    labels = [' '.join([pce_model.pce.plabels[v] for v in varlist]) for varlist in variable_interactions]
     print(type(labels[0]))
     print(variable_interactions)
     print(labels)
@@ -310,9 +314,8 @@ def SensitivityPiechart():
     return fig
 
 
-
 PLOTS = {
-    "MeanStd": MeanStd,
-    "Quantiles": Quantiles,
+    "Mean and Std": MeanStd,
+    "Quantiles Plot": Quantiles,
     "Sensitivities": SensitivityPiechart,
 }
